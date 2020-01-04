f42d4b2fd97e7d27f66cc1c62cb7cb60768ce537
Yury Antonov <yantonov@yandex.ru>
2020-01-04 15:43:02 +0500

    exit if theme is already exist

--- bin/install-themes.sh
+++ bin/install-themes.sh
@@ -5,4 +5,11 @@ cd $SCRIPT_DIR
 
 cd ..
 cd themes
+
+THEME="hugo-theme-pure"
+if [ -d "${THEME}" ]; then
+    echo "theme '${THEME}' is already exists"
+    exit 1;
+fi
+
 git clone "https://github.com/xiaoheiAh/hugo-theme-pure.git"
