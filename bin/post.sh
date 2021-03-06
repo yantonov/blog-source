c53f445b78e24a83e84e40cbb9c7a879fb9e8ad4
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2021-03-06 22:05:37 +0100

    remove non alphanumeric symbols from the filename

--- bin/post.sh
+++ bin/post.sh
@@ -10,7 +10,7 @@ fi
 
 cd ..
 
-PREPARED=$(echo $1 | tr ' ' '_')
+PREPARED=$(echo $1 | tr ' ' '_' | tr -cd '[:alnum:]_-')
 
 CURRENT_DATE=`date "+%Y-%m-%d-%H-%M"`
 POST_FILE_NAME="${CURRENT_DATE}-${PREPARED}.md"
